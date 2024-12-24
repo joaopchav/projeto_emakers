476b8fd577340c248dd64952dffa4e587bf3bde6
Joao Paulo Chaves Gonçalves Elias <jpchavesgoncalveselias@gmail.com>
2024-12-23 22:04:38 -0300

    Instalação inicial

---
+++ emakers_frontend/src/pages/RecuperarConta.jsx
@@ -0,0 +1,28 @@
+import "../global.css";
+import imagem1 from "../assets/imagem1.png";
+import setaImagem from '../assets/seta.png';
+import { Link } from "react-router-dom";
+
+function Login() {
+  return (
+    <div className="bloco-principal">
+      <div className="bloco-informações">
+        <div className="Titulo">Recuperar Conta</div>
+        <input type="text" placeholder="Email" />
+        
+        <Link to="/paginaInicial" className="seta-direita">
+          <img src={setaImagem} alt="Seta" />
+        </Link>
+
+        <Link to="/">Criar Conta</Link>
+        <Link to="/login">Fazer Login</Link>
+      </div>
+
+      <div className="bloco-imagem">
+        <img src={imagem1} alt="Imagem decorativa" />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ emakers_frontend/src/pages/Registro.jsx
@@ -0,0 +1,31 @@
+import "../global.css";
+import imagem1 from '../assets/imagem1.png';
+import setaImagem from '../assets/seta.png';
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="bloco-principal">
+      <div className="bloco-informações">
+        <div className="Titulo">Registrar</div>
+        <input type="text" placeholder="Nome de Usuário" />
+        <input type="text" placeholder="E-mail" />
+        <input type="text" placeholder="CPF" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirmar Senha" />
+
+        <Link to="/paginaInicial" className="seta-direita">
+          <img src={setaImagem} alt="Seta" />
+        </Link>
+
+        <Link to="/login">Fazer Login</Link>
+      </div>
+
+      <div className="bloco-imagem">
+        <img src={imagem1} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
